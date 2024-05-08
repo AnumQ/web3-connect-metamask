0a61fbeec52bec42fe7f0abcc2ebdba331cb6b00
Anum <hello@anum.io>
2024-05-08 22:33:13 +0200

    Remove web3 from project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
-import Web3 from "web3";
 
 function App() {
-  const [web3, setWeb3] = useState(null);
   const [accounts, setAccounts] = useState([]);
   const [error, setError] = useState(null);
 
-  const initWeb3 = async () => {
+  const connectAccount = useCallback(async () => {
     // Modern dapp browsers
     // ethereum is the object injected as property on the window object by Ethereum-enabled browser extensions like MetaMask.
     if (window.ethereum) {
-      const web3Instance = new Web3(window.ethereum);
-      setWeb3(web3Instance);
-
       // Requst account access
       try {
+        // https://docs.metamask.io/wallet/reference/eth_accounts/
         const accounts = await window.ethereum.request({
           method: "eth_requestAccounts",
+          params: [],
         });
         setAccounts(accounts);
       } catch (e) {
@@ -26,34 +23,41 @@ function App() {
         );
         console.error("Error connecting to Metamask ", e);
       }
-    }
-    // legacy dapp browsers
-    else if (window.web3) {
-      const web3Instance = new Web3(window.web3.currentProvider);
-      setWeb3(web3Instance);
-      const accounts = await web3Instance.eth.getAccounts();
-      setAccounts(accounts);
-    }
-    // Non-dapp browsers
-    else {
+    } else {
       setError(
         "Non-Ethereum browser detected. Install Metamask on browser and try again."
       );
     }
-  };
+  }, []);
+
+  const disconnectAccount = useCallback(async () => {
+    await window.ethereum.request({
+      method: "wallet_revokePermissions",
+      params: [
+        {
+          eth_accounts: {},
+        },
+      ],
+    });
+
+    setAccounts([]);
+  }, []);
 
   return (
     <div className="App">
       <main>
         <p>{error && <span>{error}</span>}</p>
-        {web3 && accounts.length > 0 && (
+        {accounts.length > 0 && (
           <>
             <h2>Successfully connected to Metamask Wallet</h2>
             <h3>Connected to account: {accounts[0]}</h3>
+            <button className="button" onClick={disconnectAccount}>
+              Disconnect Your Metamask Wallet
+            </button>
           </>
         )}
         {accounts.length === 0 && (
-          <button className="button" onClick={initWeb3}>
+          <button className="button" onClick={connectAccount}>
             Connect Your Metamask Wallet
           </button>
         )}
