b8b2f85d8d08c4e72a92fefc071101e1c7454a08
Anum <hello@anum.io>
2024-05-08 22:57:22 +0200

    Add function get Balance using Web3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import "./App.css";
+import Web3 from "web3";
 
 function App() {
+  const [web3, setWeb3] = useState(null);
   const [accounts, setAccounts] = useState([]);
   const [error, setError] = useState(null);
+  const [balance, setBalance] = useState(null);
 
-  const connectAccount = useCallback(async () => {
+  // ethereum is the object injected as property on the window object by Ethereum-enabled browser extensions like MetaMask.
+  const MetaMask = window.ethereum;
+
+  useEffect(() => {
+    if (MetaMask) {
+      const web3Instance = new Web3(MetaMask);
+      setWeb3(web3Instance);
+    }
+  }, [MetaMask]);
+
+  const connectWallet = useCallback(async () => {
     // Modern dapp browsers
-    // ethereum is the object injected as property on the window object by Ethereum-enabled browser extensions like MetaMask.
-    if (window.ethereum) {
+    if (MetaMask) {
       // Requst account access
       try {
         // https://docs.metamask.io/wallet/reference/eth_accounts/
-        const accounts = await window.ethereum.request({
+        const accounts = await MetaMask.request({
           method: "eth_requestAccounts",
           params: [],
         });
@@ -28,10 +40,10 @@ function App() {
         "Non-Ethereum browser detected. Install Metamask on browser and try again."
       );
     }
-  }, []);
+  }, [MetaMask]);
 
-  const disconnectAccount = useCallback(async () => {
-    await window.ethereum.request({
+  const disconnectWallet = useCallback(async () => {
+    await MetaMask.request({
       method: "wallet_revokePermissions",
       params: [
         {
@@ -41,7 +53,19 @@ function App() {
     });
 
     setAccounts([]);
-  }, []);
+  }, [MetaMask]);
+
+  // Get account balance
+  async function getBalance(account) {
+    try {
+      let balance = await web3.eth.getBalance(account); // returns balance in wei
+      balance = web3.utils.fromWei(balance, "ether"); // convert to ether
+      setBalance(balance);
+    } catch (e) {
+      setError("Error getting balance. See console output for details");
+      console.error("Errong getting balance. Details: ", e);
+    }
+  }
 
   return (
     <div className="App">
@@ -51,13 +75,23 @@ function App() {
           <>
             <h2>Successfully connected to Metamask Wallet</h2>
             <h3>Connected to account: {accounts[0]}</h3>
-            <button className="button" onClick={disconnectAccount}>
+            <h3>Balance: {balance}eth</h3>
+            <button
+              className="button"
+              onClick={() => {
+                getBalance(accounts[0]);
+              }}
+            >
+              Get balance
+            </button>
+            <br />
+            <button className="button" onClick={disconnectWallet}>
               Disconnect Your Metamask Wallet
             </button>
           </>
         )}
         {accounts.length === 0 && (
-          <button className="button" onClick={connectAccount}>
+          <button className="button" onClick={connectWallet}>
             Connect Your Metamask Wallet
           </button>
         )}
